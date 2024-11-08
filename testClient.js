768d7715bbc39cc8b03904274930547875d7d896
mumairtufail <mumairtufail786@gmail.com>
2024-11-08 15:10:56 +0500

    Update socket connection URL to use localhost for local development

--- testClient.js
+++ testClient.js
@@ -1,5 +1,5 @@
 const io = require('socket.io-client');
-const socket = io('http://54.82.13.151:3000');
+const socket = io('http://localhost:3000');
 
 function generateLocation(baseLocation) {
     return {
